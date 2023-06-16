3c0f26f8d2763c83f8fb486491186b3bfd5884de
vannace <sheepy00@163.com>
2023-06-17 00:42:39 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '小项老师',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'ofzjr6ZhyQVy8i3kXXRsb17dZ8t8',
+      id: 'ofzjr6TnP4lcyvQhge-gbcbNnbc0',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'WY_5YN20jDRoftADAseV2i_uLwyYpPzDMcAOrBrUxmU',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD

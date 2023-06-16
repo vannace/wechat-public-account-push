5151ebc147355c9ab93c0f4d221119a6460dbbb0
vannace <sheepy00@163.com>
2023-06-17 00:30:07 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -42,9 +42,9 @@ const USER_CONFIG = {
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-11-12' },
+        { keyword: 'love_day', date: '2022-07-14' },
         // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-07-14' },
+        { keyword: 'marry_day', date: '2022-11-12' },
       ],
     },
   ],
